3222b8f2b948b870c6482251817bd1d25ade5bc2
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-08-24 02:21:02 -0700

    update on console file message

--- console.py
+++ console.py
@@ -34,7 +34,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 0:
             print("** class name missing **")
         elif args[0] not in HBNBCommand.__classes:
-            print("** class name missing **")
+            print("** class doesn't exist **")
         else:
             new_creation = eval(args[0] + '()')
             models.storage.save()
