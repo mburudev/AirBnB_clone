370140c42e4590356ac1f6a52094d749bf0bab5e
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-08-24 03:34:14 -0700

    console

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """ Console Module """
 import cmd
 import sys
+import shlex
+import models
+from datetime import datetime
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
